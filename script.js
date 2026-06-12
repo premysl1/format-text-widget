7255dfa59d1fbedd048a183c88688ec07a97a7ce
remy <premysl.vacek@outlook.com>
2026-06-12 16:16:32 +0200

    refactor + redesign

--- script.js
+++ script.js
@@ -1,80 +1,79 @@
-const textSettings = document.getElementById(`text-settings`);
-const textPositionSlider = document.getElementById(`text-position-slider`);
-const textSettingsSliders = document.querySelectorAll(`.text-settings-slider`);
-const textWidthSlider = document.getElementById(`text-width-slider`);
-const changeFontSize = document.getElementById(`change-font-size`);
-const fontSizeIncrease = document.getElementById(`font-size-increase`);
-const fontSizeDecrease = document.getElementById(`font-size-decrease`);
-const content = document.getElementById(`content`);
-const main = document.querySelector(`main`);
+const textSettingsToggle = document.querySelector(`#text-settings-widget > button`);
+const textSettingsWidget = document.querySelector(`#text-settings-widget > menu`);
 
-let textSettingsExpanded = false;
-textSettings.addEventListener(`click`, () =>
-    {
-        if (!textSettingsExpanded) {
-            textSettingsExpanded = true;
+textSettingsToggle.addEventListener(`click`, () =>
+{
+    if (textSettingsToggle.getAttribute(`aria-pressed`) === `false`) {
+        textSettingsToggle.setAttribute(`aria-pressed`, `true`);
+        
+        textSettingsWidget.style.display = `grid`;
 
-            textSettings.style.background = `lightgray`;
-            
-            textSettingsSliders[0].style.display = `none`;
-            textSettingsSliders[1].style.display = `none`;
-            changeFontSize.style.display = `none`;
-        } else {
-            textSettings.style.background = `darkgrey`;
-            
-            textSettingsSliders[0].style.display = `flex`;
-            textSettingsSliders[1].style.display = `flex`;
-            changeFontSize.style.display = `flex`;
+        textSettingsToggle.style.background = `black`;
+    } else {
+        textSettingsToggle.setAttribute(`aria-pressed`, `false`);
 
-            textSettingsExpanded = false;
-        }
+        textSettingsWidget.style.display = `none`;
+
+        textSettingsToggle.style.background = `rgb(75, 76, 83)`;
     }
+}
 );
 
-textSettings.dispatchEvent(new Event(`click`));
+const textPositionSlider = document.getElementById(`text-position-slider`);
+const content = document.getElementById(`content`);
 
 textPositionSlider.addEventListener(`input`, e =>
-    {
-        const value = Number(e.target.value);
+{
+    const value = Number(e.target.value);
 
-        const max = 100;
-        const t = (value - 1) / (max - 1);
+    const max = 100;
+    const t = (value - 1) / (max - 1);
 
-        const leftGrow = t;
-        const rightGrow = 1 - t;
+    const leftGrow = t;
+    const rightGrow = 1 - t;
 
-        content.style.setProperty(`--left-grow`, leftGrow);
-        content.style.setProperty(`--right-grow`, rightGrow);
-    }
+    content.style.setProperty(`--left-grow`, leftGrow);
+    content.style.setProperty(`--right-grow`, rightGrow);
+}
 );
 
 textPositionSlider.dispatchEvent(new Event(`input`));
 
+const textWidthSlider = document.getElementById(`text-width-slider`);
+const main = document.querySelector(`main`);
+
 textWidthSlider.addEventListener(`input`, e =>
-    {
-        main.style.width = e.target.value + `%`;
-    }
+{
+    main.style.width = e.target.value + `%`;
+}
 );
+
+const fontSizeIncrease = document.getElementById(`font-size-increase`);
+const fontSizeDecrease = document.getElementById(`font-size-decrease`);
 
 const startingFontSize = parseFloat(window.getComputedStyle(main).fontSize);
 let currentFontSize;
 
 fontSizeIncrease.addEventListener(`click`, () =>
-    {
-        currentFontSize = parseFloat(window.getComputedStyle(main).fontSize);
-        if (currentFontSize >= (startingFontSize * 4)) {
-            return;
-        }
-        main.style.fontSize = `${currentFontSize + 4}px`;
+{
+    currentFontSize = parseFloat(window.getComputedStyle(main).fontSize);
+
+    if (currentFontSize >= (startingFontSize * 4)) {
+        return;
     }
+
+    main.style.fontSize = `${currentFontSize + 4}px`;
+}
 );
 
 fontSizeDecrease.addEventListener(`click`, () =>
-    {
-        currentFontSize = parseFloat(window.getComputedStyle(main).fontSize);
-        if (currentFontSize <= (startingFontSize * 0.25)) {
-            return;
-        }
-        main.style.fontSize = `${currentFontSize - 4}px`;
+{
+    currentFontSize = parseFloat(window.getComputedStyle(main).fontSize);
+
+    if (currentFontSize <= (startingFontSize * 0.25)) {
+        return;
     }
+    
+    main.style.fontSize = `${currentFontSize - 4}px`;
+}
 );
